9998ab669d6ee5aca52ff283c3e225331cd4edc6
Anusha <anushamr141022@gmail.com>
2021-07-18 15:14:45 +0530

    add basic board

---
+++ ui/whity-board/src/components/board/Board.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+class Board extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <canvas className="board" id="board"></canvas>
+        )
+    }
+
+}
+
+export default Board
